c8eb862ae95334dc894a79a5bd8f1357f749c036
RealmKoinAdmin <31043497+RealmKoinAdmin@users.noreply.github.com>
2018-06-04 16:44:17 -0400

    Create Beta-Socket-Grid.py
    Test Item Socket Grid.

---
+++ Beta-Socket-Grid.py
@@ -0,0 +1,158 @@
+############################################################################
+#~Koinage Runes And Codex Information~
+#/// AUTHOR : Skrypt [SkrypTEk]
+#(Format Is As Follows)
+#(Name = Color,Weight,Tier,Armor,Weapon,Combine,RkValue,EthereumValue,Effect)
+############################################################################
+# Ei = Sky,1,1,True,True,True,1,0.001,+2% ^KoinFoes
+# Pi = Aqua,2,2,True,True,True,4,0.004,+3.14% ^KoinFoes
+# Zi = Ultramarine,3,3,True,True,True,16,0.016,+10% ^KoinFoes
+# Li = Periwinkle,4,4,True,True,False,64,0.064,+25% ^KoinFoes
+# E're = Celadon,1,1,False,True,True,1,0.001,+2% ^Damage
+# P're = Emerald,2,2,False,True,True,4,0.004,+3% ^Damage
+# Z're = Feldgrau,3,3,False,True,True,16,0.016,+10% ^Damage
+# L're = Malachite,4,4,False,True,False,64,0.064,+25% ^Damage
+# E'ae = Caramine,1,1,True,False,True,1,0.001,+2% ^Defense
+# P'ae = Crimson,2,2,True,False,True,4,0.004,+3% ^Defense
+# Z'ae = Maroon,3,3,True,False,True,16,0.016,+10% ^Defense
+# L'ae = Cardinal,4,4,True,False,False,64,0.064,+25% ^Defense
+# E'ai = Purpureus,1,1,True,False,True,1,0.001,+1 MaxStrength
+# P'ai = Eminence,2,2,True,False,True,4,0.004,+1 MaxCharisma
+# Z'ai = Palatinate,3,3,True,False,True,16,0.016,+1 MaxAgility
+# S'ai = Heliotrope,4,4,True,False,False,64,0.064,+1 MaxIntelligence
+# Kel = Ebony,5,5,False,False,False,100,0.100,Remove [Ei,Pi,Zi,Li]
+# Ael = Taupe,5,5,False,False,False,100,0.100,Remove [E're,P're,Z're,L're]
+# Tel = Charcoal,5,5,False,False,False,100,0.100,Remove [E'ae,P'ae,Z'ae,L'ae]
+# Sel = Jet,5,5,False,False,False,100,0.100,Remove [E'ai,P'ai,Z'ai,S'ai]
+# Del = Onyx,5,5,False,False,False,500,0.500,Remove [LastSocket]
+#############################################################################
+#############################################################################
+#~Weapon Runes~##############################################################
+#[Ei,Pi,Zi,Li,E're,P're,Z're,L're]
+#############################################################################
+#~Weapon Attributes~#########################################################
+#[+%^KoinFoes,+%^Damage]
+#############################################################################
+#~Armor Runes~###############################################################
+#[E'ae,P'ae,Z'ae,L'ae,E'ai,P'ai,Z'ai,S'ai]
+#############################################################################
+#~Armor Attributes~##########################################################
+#[+%^Defense,+1*StatMax]
+#############################################################################
+#############################################################################
+#############################################################################
+#~Koinage Weapons Codex Information/Sockets~
+#/// AUTHOR : Skrypt [SkrypTEk, GuildSkrypt]
+#############################################################################
+#Per Socket Type List: #Dev List ############################################
+#############################################################################
+#1 Socket (8)
+#2 Sockets (64)
+#3 Sockets (512)
+#4 Sockets (4096)
+#5 Sockets (32,768)
+#6 Sockets (262,144)
+#############################################################################
+#Total Types Per Socket Per Item: #Dev List #################################
+#############################################################################
+#1 Socket = (8) Types
+#2 Sockets = (72) Types
+#3 Sockets = (584) Types
+#4 Sockets = (4,680) Types
+#5 Sockets = (37,448) Types
+#6 Sockets = (299,592) Types
+#############################################################################
+#############################################################################
+#############################################################################
+######################~(Weapon)'S~###########################################
+#######################~(BELOW)~#############################################
+#############################################################################
+########## START (1) SOCKET LIST-Types(8) ###################################
+#############################################################################
+Ei = "Ei (Weapon)" #+2% Increased Koin Drops / Shattered
+Pi = "Pi (Weapon)" #+3.14% Increased Koin Drops / Cracked
+Zi = "Zi (Weapon)" #+10% Increased Koin Drops / Pitted
+Li = "Li (Weapon)" #+25% Increased Koin Drops / Magnetic
+Ere = "E're (Weapon)" #+2% Enhanced Damage / Dull
+Pre = "P're (Weapon)" #+3% Enhanced Damage / Chipped
+Zre = "Z're (Weapon)" #+10% Enhanced Damage / Sharp
+Lre = "L're (Weapon)" #+25% Enhanced Damage / Piercing
+#############################################################################
+################# END (1) SOCKET LIST #######################################
+#############################################################################
+############# START (2) SOCKET LIST-Types(64)################################
+########## Ei+[*] ###########################################################
+Ei+Ei = "Eis (Weapon)" #+4% Increased Koin Drops 
+Ei+Pi = "Epi (Weapon)" #+5.14% Increased Koin Drops
+Ei+Zi = "Ezi (Weapon)" #+12% Increased Koin Drops
+Ei+Li = "Eli (Weapon)" #+27% Increased Koin Drops
+Ei+Ere = "E'Ere (Weapon)" #+2% Increased Koin Drops, +2% Enhanced Damage
+Ei+Pre = "E'Pre (Weapon)" #+2% Increased Koin Drops, +3% Enhanced Damage
+Ei+Zre = "E'Zre (Weapon)" #+2% Increased Koin Drops, +10% Enhanced Damage
+Ei+Lre = "E'Lre (Weapon)" #+2% Increased Koin Drops, +25% Enhanced Damage
+########## Pi+[*] ###########################################################
+Pi+Ei = "Pei (Weapon)" #+5.14% Increased Koin Drops
+Pi+Pi = "Pis (Weapon)" #+6.24% Increased Koin Drops
+Pi+Zi = "Pzi (Weapon)" #+13.14% Increased Koin Drops
+Pi+Li = "Pli (Weapon)" #+28.14% Increased Koin Drops
+Pi+Ere = "P'Ere (Weapon)" #+3.14% Increased Koin Drops, +2% Enhanced Damage
+Pi+Pre = "P'Pre (Weapon)" #+3.14% Increased Koin Drops, +3% Enhanced Damage
+Pi+Zre = "P'Zre (Weapon)" #+3.14% Increased Koin Drops, +10% Enhanced Damage
+Pi+Lre = "P'Lre (Weapon)" #+3.14% Increased Koin Drops, +25% Enhanced Damage
+########## Zi+[*] ###########################################################
+Zi+Ei = "Zei (Weapon)" #+12% Increased Koin Drops
+Zi+Pi = "Zpi (Weapon)" #+13.14% Increased Koin Drops
+Zi+Zi = "Zis (Weapon)" #+20% Increased Koin Drops
+Zi+Li = "Zli (Weapon)" #+35% Increased Koin Drops
+Zi+Ere = "Z'Ere (Weapon)" #+10% Increased Koin Drops, +2% Enhanced Damage
+Zi+Pre = "Z'Pre (Weapon)" #+10% Increased Koin Drops, +3% Enhanced Damage
+Zi+Zre = "Z'Zre (Weapon)" #+10% Increased Koin Drops, +10% Enhanced Damage
+Zi+Lre = "Z'Lre (Weapon)" #+10% Increased Koin Drops, +25% Enhanced Damage
+########## Li+[*] ###########################################################
+Li+Ei = "Lei (Weapon)" #+27% Increased Koin Drops
+Li+Pi = "Lpi (Weapon)" #+28.14 Increased Koin Drops
+Li+Zi = "Lzi (Weapon)" #+35% Increased Koin Drops
+Li+Li = "Lis (Weapon)" #+50% Increased Koin Drops
+Li+Ere = "L'Ere (Weapon)" #+25% Increased Koin Drops, +2% Enhanced Damage
+Li+Pre = "L'Pre (Weapon)" #+25% Increased Koin Drops, +3% Enhanced Damage
+Li+Zre = "L'Zre (Weapon)" #+25% Increased Koin Drops, +10% Enhanced Damage
+Li+Lre = "L'Lre (Weapon)" #+25% Increased Koin Drops, +25% Enhanced Damage
+########## E're+[*] #########################################################
+Ere+Ei = "Ere'ei (Weapon)" #+2% Enhanced Damage, +2% Increased Koin Drops
+Ere+Pi = "Ere'pi (Weapon)" #+2% Enhanced Damage, +3.14 Increased Koin Drops
+Ere+Zi = "Ere'zi (Weapon)" #+2% Enhanced Damage, +10% Increased Koin Drops
+Ere+Li = "Ere'li (Weapon)" #+2% Enhanced Damage, +25% Increased Koin Drops
+Ere+Ere = "E'res (Weapon)" #+4% Enhanced Damage
+Ere+Pre = "Ere'Pre (Weapon)" #+7% Enhanced Damage
+Ere+Zre = "Ere'Zre (Weapon)" #+12% Enhanced Damage
+Ere+Lre = "Ere'Lre (Weapon)" #+27% Enhanced Damage
+########## P're+[*] #########################################################
+Pre+Ei = "Pre'ei (Weapon)" #+3% Enhanced Damage, +2% Increased Koin Drops
+Pre+Pi = "Pre'pi (Weapon)" #+3% Enhanced Damage, +3.14% Increased Koin Drops
+Pre+Zi = "Pre'zi (Weapon)" #+3% Enhanced Damage, +10% Increased Koin Drops
+Pre+Li = "Pre'li (Weapon)" #+3% Enhanced Damage, +25% Increased Koin Drops
+Pre+Ere = "Pre'Ere (Weapon)" #+6% Enhanced Damage
+Pre+Pre = "P'res (Weapon)" #+9.14% Enhanced Damage
+Pre+Zre = "Pre'Zre (Weapon)" #+13% Enhanced Damage
+Pre+Lre = "Pre'Lre (Weapon)" #+28% Enhanced Damage
+########## Z're+[*] #########################################################
+Zre+Ei = "Zre'ei (Weapon)" #+10% Enhanced Damage, +2% Increased Koin Drops
+Zre+Pi = "Zre'pi (Weapon)" #+10% Enhanced Damage, +3.14% Increased Koin Drops
+Zre+Zi = "Zre'zi (Weapon)" #+10% Enhanced Damage, +10% Increased Koin Drops
+Zre+Li = "Zre'li (Weapon)" #+10% Enhanced Damage, +25% Increased Koin Drops
+Zre+Ere = "Zre'Ere (Weapon)" #+12% Enhanced Damage
+Zre+Pre = "Zre'Pre (Weapon)" #+13.14% Enhanced Damage
+Zre+Zre = "Z'res (Weapon)" #+20% Enhanced Damage
+Zre+Lre = "Zre'Lre (Weapon)" #+35% Enhanced Damage
+########## L're+[*] #########################################################
+Lre+Ei = "Lre'ei (Weapon)" #+25% Enhanced Damage, +2% Increased Koin Drops
+Lre+Pi = "Lre'pi (Weapon)" #+25% Enhanced Damage, +3.14% Increased Koin Drops
+Lre+Zi = "Lre'zi (Weapon)" #+25% Enhanced Damage, +10% Increased Koin Drops
+Lre+Li = "Lre'li (Weapon)" #+25% Enhanced Damage, +25% Increased Koin Drops
+Lre+Ere = "Lre'Ere (Weapon)" #+27% Enhanced Damage
+Lre+Pre = "Lre'Pre (Weapon)" #+28.14% Enhanced Damage
+Lre+Zre = "Lre'Zre (Weapon)" #+35% Enhanced Damage
+Lre+Lre = "L'res (Weapon)" #+50% Enhanced Damage
+###############################################################################
+#################### END (2) Socket List ######################################
+###############################################################################
